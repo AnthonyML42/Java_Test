0bbcff97365e048d97394fbf61f01ffb1edfd21b
Anthony <anthony.m.lawrence42@gmail.com>
2017-02-24 11:27:16 +0000

    added new public

--- Java_Test/src/Class1.java
+++ Java_Test/src/Class1.java
@@ -1,9 +1,13 @@
-
+import java.util.*;
 public class Class1 {
 
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World!!");
 	}
-
+	
+	public static final double FAVNUMBER main(String[] args)
+	{
+		
+	}
 }
