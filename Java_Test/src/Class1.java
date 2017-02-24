383b8f874436f492e0bc33c7bd24f266a9b76fbc
Anthony <anthony.m.lawrence42@gmail.com>
2017-02-24 17:47:13 +0000

    switched tutorial

--- Java_Test/src/Class1.java
+++ Java_Test/src/Class1.java
@@ -5,22 +5,4 @@ public class Class1
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World!!");
-	}
-	
-	public static final double FAVNUMBER = 1.6180;
-	{
-		private String name;
-		private int level;
-		private boolean has level 100 = false;
-		private byte currentlvl;
-		private long PersonalID;
-		private char FavLvl;
-		private double lvlspeed
-		private float lvlspeed2
-		
-		protected static int numberoflvls = 0;
-		
-		static Scanner userinput = bey Scanner
-		
-	}
 }
