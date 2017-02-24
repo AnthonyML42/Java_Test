b93b538f784895a1ffcc66e8df4b52700cee5f1a
Anthony <anthony.m.lawrence42@gmail.com>
2017-02-24 13:31:36 +0000

    added tutorial features

--- Java_Test/src/Class1.java
+++ Java_Test/src/Class1.java
@@ -1,13 +1,17 @@
 import java.util.*;
-public class Class1 {
+public class Class1 
+{
 
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World!!");
 	}
 	
-	public static final double FAVNUMBER main(String[] args)
+	public static final double FAVNUMBER = 1.6180;
 	{
-		
+		private String name;
+		private int level;
+		private boolean has level 100 = false;
+		private byte currentlvl;
 	}
 }
