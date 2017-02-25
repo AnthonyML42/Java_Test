2e2b1a370e99a17493f3e141deaa60f94f64cf43
Anthony <anthony.m.lawrence42@gmail.com>
2017-02-25 18:35:30 +0000

    added things in class.file

--- Java_Test/src/Class1.java
+++ Java_Test/src/Class1.java
@@ -1,13 +1,13 @@
 import java.util.*;
 public class Class1 
-{
 
-	public static void main(String[] args) 
-	{
-		System.out.println("Hello World!!");
+
+public static void main(String[] args) 
+{
+	System.out.println("Hello World!!");
 		
-		int x = 10;
-		int y =  3;
-		int result = x % y;
-		System.out.println("result = " + result);
+	int x = 10;
+	int y =  3;
+	int result = x % y;
+	System.out.println("result = " + result);
 }
