e37b0ea2cbbd319a2770e2acdb9f4e51acd2bb9e
Anthony <anthony.m.lawrence42@gmail.com>
2017-02-25 18:15:33 +0000

    added things in class.file

--- Java_Test/src/Class1.java
+++ Java_Test/src/Class1.java
@@ -5,4 +5,9 @@ public class Class1
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World!!");
+		
+		int x = 10;
+		int y =  3;
+		int result = x % y;
+		System.out.plintln("result = " + result);
 }
